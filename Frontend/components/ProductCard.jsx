a672bfe7aa667d4986fbaca473064bde0cd113ce
Lennox Matwere <aberamacarleton@gmail.com>
2023-08-20 16:43:16 -0700

    Add to cart button

--- Frontend/components/ProductCard.jsx
+++ Frontend/components/ProductCard.jsx
@@ -21,7 +21,7 @@ const ProductCard = ({ data }) => {
       className="cursor-pointer hover:scale-105 duration-300 overflow-hidden transform"
       style={{
         width: "350px", // Set a fixed width for the card
-               display: "inline-block", // Make the cards appear next to each other
+        display: "inline-block", // Make the cards appear next to each other
         boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)", // Add a soft shadow
         borderRadius: "8px", // Rounded corners
       }}
@@ -43,8 +43,31 @@ const ProductCard = ({ data }) => {
       </div>
       <div className="p-4 text-black">
         <h2 className="text-lg font-extra-bold">{name}</h2>
+
         <div className="flex items-center text-black">
           <p className="mr-2 text-lg text-green-600 font-bold">{`$${price}`}</p>
+          <button
+            href={`/product/${slug}`}
+            class="flex items-center mt-3 rounded-md bg-[#BF330E]  px-4 py-3 ml-24 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          
+          
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="mr-2 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+            Add to cart
+          </button>
           {original_price && (
             <>
               <p className="text-base font-medium line-through">{`$${original_price}`}</p>
