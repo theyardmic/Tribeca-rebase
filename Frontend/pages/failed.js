8f2a2bcd8188c84aee8df6df6191ad205da5763f
Lennox Matwere <aberamacarleton@gmail.com>
2023-08-19 23:43:58 -0700

    alerts branding

--- Frontend/pages/failed.js
+++ Frontend/pages/failed.js
@@ -6,14 +6,14 @@ const Failed = () => {
     return (
         <div className="min-h-[650px] flex items-center">
             <Wrapper>
-                <div className="max-w-[600px] rounded-lg p-5 border border-black mx-auto flex flex-col">
-                    <div className="text-2xl font-bold">Payment failed!</div>
+                <div className="max-w-[600px] rounded-lg p-5 border border-[#ff0000]/[1]  mx-auto flex flex-col">
+                    <div className="text-2xl text-[#ff0000]/[1]  font-bold">Payment failed!</div>
                     <div className="text-base mt-5">
                         For any product related query, drop an email to
                     </div>
                     <div className="underline">tribeckasupport@shop.com</div>
 
-                    <Link href="/" className="font-bold mt-5">
+                    <Link href="/" className="font-bold text-[#008000]/[1]  mt-5">
                         Continue Shopping
                     </Link>
                 </div>
--- Frontend/pages/success.js
+++ Frontend/pages/success.js
@@ -6,11 +6,11 @@ const Success = () => {
     return (
         <div className="min-h-[650px] flex items-center">
             <Wrapper>
-                <div className="max-w-[600px] rounded-lg p-5 border border-black mx-auto flex flex-col">
+                <div className="max-w-[600px] rounded-lg p-5 border border-[#008000]/[100]  mx-auto flex flex-col">
                     <div className="text-2xl font-bold">
-                        Thanks for shopping with us!
+                        Thank you for shopping with us!
                     </div>
-                    <div className="text-lg font-bold mt-2">
+                    <div className="text-lg text-[#008000]/[1]  font-bold mt-2">
                         Your order has been placed successfully.
                     </div>
                     <div className="text-base mt-5">
@@ -18,7 +18,7 @@ const Success = () => {
                     </div>
                     <div className="underline">tribeckasupport@shop.com</div>
 
-                    <Link href="/" className="font-bold mt-5">
+                    <Link href="/" className="font-bold text-[#008000]/[1]  mt-5">
                         Continue Shopping
                     </Link>
                 </div>
