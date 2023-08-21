5dc4ab76eefe1ae2e80733f973443297fec0a2c0
Lennox Matwere <aberamacarleton@gmail.com>
2023-08-20 17:05:57 -0700

    Features, Top Categories

---
+++ Frontend/components/TopSellers.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import ProductCard from "./ProductCard";
+
+const TopCategories = ({ products }) => {
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1023, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 767, min: 0 },
+            items: 1,
+        },
+    };
+
+    return (
+        <div className="mt-[50px] md:mt-[100px] mb-[100px] md:mb-0 ">
+    
+            <Carousel
+                responsive={responsive}
+                containerClass="mx-[10px]"
+                itemClass="px-[10px]"
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={5000}
+                keyBoardControl={true}
+                transitionDuration={500}
+                
+            >
+                {products?.data?.map((product) => (
+                    <ProductCard key={product?.id} data={product} />
+                ))}
+            </Carousel>
+        </div>
+    );
+};
+
+export default TopCategories;
+
+export async function getStaticPaths() {
+    const products = await fetchDataFromAPi("/api/products?populate=*");
+    const paths = products?.data?.map((p) => ({
+      params: {
+        slug: p.attributes.slug,
+      },
+    }));
+  
+    return {
+      paths,
+      fallback: false,
+    };
+  }
+  
+  export async function getStaticProps({ params: { slug } }) {
+    const product = await fetchDataFromAPi(
+      `/api/products?populate=*&filters[slug][$eq]=${slug}`
+    );
+    const products = await fetchDataFromAPi(
+      `/api/products?populate=*&[filters][slug][$ne]=${slug}`
+    );
+  
+    return {
+      props: {
+        product,
+        products,
+      },
+    };
+  }
--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -4,6 +4,9 @@ import Wrapper from "@/components/Wrapper";
 import ProductCard from "@/components/ProductCard";
 import { fetchDataFromAPi } from "@/utils/api";
 import { useState, useEffect } from "react";
+import TopCategories from "@/components/TopSellers";
+import Features from "@/components/Features";
+
 
 export default function Home({ products }) {
   const [data, setData] = useState(null);
@@ -22,7 +25,7 @@ export default function Home({ products }) {
 
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
-          <div className="text-2xl md:text-xl  lg:text-3xl  text-[#BF330E]  mb-5 font-bold lading-tight">
+          <div className="text-2xl md:text-xl  lg:text-3xl  text-[#BF330E]  mb-5 font-bold leading-tight">
             Discover the Treasure of Africa at African Tribeca
           </div>
           <div className="text-md md:text-xl">
@@ -33,13 +36,20 @@ export default function Home({ products }) {
             don't worry, that's not the case with us.
           </div>
         </div>
-        {/* Product Cards */}
+
+        {/*
+         Product Cards 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
           {products?.data?.map((product) => (
             <ProductCard key={product?.id} data={product} />
           ))}
         </div>
-    
+      */}
+     <div className="text-2xl md:text-xl  lg:text-3xl  text-[#BF330E]  mb-5 font-bold leading-tight">
+        <h1>Our Top Sellers</h1>   
+        < TopCategories products={products}/>
+          </div>
+          <Features />
       </Wrapper>
     </main>
   );
@@ -49,3 +59,5 @@ export async function getStaticProps() {
   const products = await fetchDataFromAPi("/api/products?populate=*");
   return { props: { products } };
 }
+
+
